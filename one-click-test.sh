c6741657281443fdaec646331364e8efe43ff7e0
singhigh <singhigh@users.noreply.github.com>
2016-03-29 01:50:15 +0800

    Create one-click-test.sh

---
+++ one-click-test.sh
@@ -0,0 +1,72 @@
+# ONE CLICK ALL
+# USAGE :wget --no-check-certificate https://raw.githubusercontent.com/singhigh/Speedtest-cli-CN-Auto/master/one-click-test.sh && bash one-click-test.sh {shanghai|telecom|unicom|mobile|all}
+
+dir=`basename $0`
+if [ $1 == "shanghai" ];then
+  # GET speedtest-cli
+  wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py 
+  chmod a+rx speedtest_cli.py 
+  sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli 
+  sudo chown root:root /usr/local/bin/speedtest-cli 
+  # GET speedtest-script
+  wget --no-check-certificate https://raw.githubusercontent.com/singhigh/Speedtest-cli-CN-Auto/master/speedtest-choice.sh 
+  # RUN script
+  bash speedtest-choice.sh shanghai
+  # DELETE speedtest-cli and script
+  rm -rf speedtest-choice.sh
+  rm -rf /usr/local/bin/speedtest-cli
+elif [ $1 == "telecom" ];then
+  # GET speedtest-cli
+  wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py 
+  chmod a+rx speedtest_cli.py 
+  sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli 
+  sudo chown root:root /usr/local/bin/speedtest-cli 
+  # GET speedtest-script
+  wget --no-check-certificate https://raw.githubusercontent.com/singhigh/Speedtest-cli-CN-Auto/master/speedtest-choice.sh 
+  # RUN script
+  bash speedtest-choice.sh telecom
+  # DELETE speedtest-cli and script
+  rm -rf speedtest-choice.sh
+  rm -rf /usr/local/bin/speedtest-cli
+elif [ $1 == "unicom" ];then
+  # GET speedtest-cli
+  wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py 
+  chmod a+rx speedtest_cli.py 
+  sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli 
+  sudo chown root:root /usr/local/bin/speedtest-cli 
+  # GET speedtest-script
+  wget --no-check-certificate https://raw.githubusercontent.com/singhigh/Speedtest-cli-CN-Auto/master/speedtest-choice.sh 
+  # RUN script
+  bash speedtest-choice.sh unicom
+  # DELETE speedtest-cli and script
+  rm -rf speedtest-choice.sh
+  rm -rf /usr/local/bin/speedtest-cli
+elif [ $1 == "mobile" ];then
+  # GET speedtest-cli
+  wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py 
+  chmod a+rx speedtest_cli.py 
+  sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli 
+  sudo chown root:root /usr/local/bin/speedtest-cli 
+  # GET speedtest-script
+  wget --no-check-certificate https://raw.githubusercontent.com/singhigh/Speedtest-cli-CN-Auto/master/speedtest-choice.sh 
+  # RUN script
+  bash speedtest-choice.sh mobile
+  # DELETE speedtest-cli and script
+  rm -rf speedtest-choice.sh
+  rm -rf /usr/local/bin/speedtest-cli
+elif [ $1 == "all" ];then
+    # GET speedtest-cli
+  wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py 
+  chmod a+rx speedtest_cli.py 
+  sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli 
+  sudo chown root:root /usr/local/bin/speedtest-cli 
+  # GET speedtest-script
+  wget --no-check-certificate https://raw.githubusercontent.com/singhigh/Speedtest-cli-CN-Auto/master/speedtest-choice.sh 
+  # RUN script
+  bash speedtest-choice.sh all
+  # DELETE speedtest-cli and script
+  rm -rf speedtest-choice.sh
+  rm -rf /usr/local/bin/speedtest-cli
+else
+   echo "$dir.sh {shanghai|telecom|unicom|mobile|all}"
+fi
