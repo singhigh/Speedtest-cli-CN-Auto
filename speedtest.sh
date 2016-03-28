3b0f1a18b38fbee189e6788899914d0a3aabdcf4
singhigh <singhigh@users.noreply.github.com>
2016-03-28 21:07:50 +0800

    bug fixed & polished the import

--- speedtest.sh
+++ speedtest.sh
@@ -1,181 +1,220 @@
 #Credit to speedtest-cli
 
 echo ===========================================
--e “\033[41;33m Speedtest-cli-CN-Auto Shell by singhigh @ CCF 
+echo Speedtest-cli-CN-Auto Shell by singhigh @ CCF 
+echo 
+echo https://github.com/singhigh/Speedtest-cli-CN-Auto
 echo ===========================================
 #Show nearest ISP connection
 echo ===========================================
-echo -e “\033[41;33m Now test connection to the NEAREST location based on your IP 
+echo Now test connection to the NEAREST location based on your IP 
 echo ===========================================
 speedtest-cli --bytes
 #Show connection to China
 echo ===========================================
-echo -e “\033[41;33m Now test connection to China Telecom Hosts 
+echo Now test connection to China Telecom Hosts 
 echo ===========================================
 echo  Shanghai Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 3633
 echo ===========================================
 echo  Beijing Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 4751
 echo ===========================================
 echo  Guangzhou Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 4672
 echo ===========================================
 echo  Hangzhou Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 7509
 echo ===========================================
 echo  Hefei Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 4433
 echo ===========================================
 echo  Nanjing Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 5316
 echo ===========================================
 echo  Chongqing Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 6592
 echo ===========================================
 echo  Lanzhou Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 3973
 echo ===========================================
 echo  Xiangyang(Hubei) Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 6435
 echo ===========================================
 echo  Nanchang Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 7643
 echo ===========================================
 echo  Kunming Telecom 
+echo ===========================================
 speedtest-cli --bytes --server 6415
 echo ===========================================
 echo China Telecom Speedtest Finished 
 echo ===========================================
 echo 
 echo ===========================================
-echo -e “\033[41;33m Now test connection to China Unicom Hosts 
+echo Now test connection to China Unicom Hosts 
 echo ===========================================
 echo  Shanghai Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5083
 echo ===========================================
 echo  Guangzhou Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 3891
 echo ===========================================
 echo  Hangzhou Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5300
 echo ===========================================
 echo  Hohhot Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5465
 echo ===========================================
 echo  Tianjin Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5475
 echo ===========================================
 echo  Jinan Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5039
 echo ===========================================
 echo  Xining(Qinghai) Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5992
 echo ===========================================
 echo  Ningxia Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5509
 echo ===========================================
 echo  Zhengzhou Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5131
 echo ===========================================
 echo  Wuhan Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 7232
 echo ===========================================
 echo  Lhasa Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5750
 echo ===========================================
 echo  Jiangxi Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5097
 echo ===========================================
 echo  Changsha Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 4870
 echo ===========================================
 echo  Xiamen Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5506
 echo ===========================================
 echo  Nanning Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5647
 echo ===========================================
 echo  Haikou Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5985
 echo ===========================================
 echo  Harbin Unicom 
+echo ===========================================
 speedtest-cli --bytes --server 5460
 echo ===========================================
 echo China Unicom Speedtest Finished 
 echo ===========================================
 echo 
 echo ===========================================
-echo -e “\033[41;33m Now test connection to China Mobile Hosts 
+echo Now test connection to China Mobile Hosts 
 echo ===========================================
 echo  Beijing Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 4713
 echo ===========================================
 echo  Shanghai Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 4665
 echo ===========================================
 echo  Guangzhou Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 6611
 echo ===========================================
 echo  Hangzhou Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 4647
 echo ===========================================
 echo  Ningbo Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 6715
 echo ===========================================
 echo  Kunming Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 5892
 echo ===========================================
-echo  Beijing Mobile 
-speedtest-cli --bytes --server 4713
-echo ===========================================
 echo  Shenzhen Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 4515
 echo ===========================================
 echo  Lanzhou Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 4504
 echo ===========================================
 echo  Zhengzhou Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 4486
 echo ===========================================
 echo  Xi'An Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 5292
 echo ===========================================
 echo  Hefei Mobile 
+echo ===========================================
 speedtest-cli --bytes --server 4377
 echo ===========================================
 echo  Wuxi(Jiangsu) Mobile 
 speedtest-cli --bytes --server 5122
 echo ===========================================
-echo  Beijing Mobile 
-speedtest-cli --bytes --server 4713
-echo ===========================================
 echo China Mobile Speedtest Finished 
 echo ===========================================
 echo 
 echo ===========================================
-echo -e “\033[41;33m Now test connection to Hongkong, Macau and Taiwan 
+echo Now test connection to Hongkong, Macau and Taiwan 
 echo ===========================================
 echo  Atombase Global Datamation, Hongkong 
+echo ===========================================
 speedtest-cli --bytes --server 8170
 echo ===========================================
 echo speedtest-cli Website Solution Limited, Hongkong 
 speedtest-cli --bytes --server 2993
 echo ===========================================
 echo  Macau CTM 
+echo ===========================================
 speedtest-cli --bytes --server 1849
 echo ===========================================
 echo  Far EasTone Telecommunications, Taiwan, China 
+echo ===========================================
 speedtest-cli --bytes --server 2589
 echo ===========================================
 echo  Taipei Fiber, Taiwan, China 
+echo ===========================================
 speedtest-cli --bytes --server 5056
 echo ===========================================
 echo Hong Kong, Macau and Taiwan Speedtest Finished 
 echo ===========================================
 
 echo ===========================================
-echo -e “\033[41;33m Speedtest Completed 
+echo          Speedtest Completed 
 echo ===========================================
 
 
