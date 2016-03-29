c55e436d4d5e7de630e0850aa4aeb7a7dfacd414
singhigh <singhigh@users.noreply.github.com>
2016-03-30 03:15:21 +0800

    Update install-2.sh

--- install-2.sh
+++ install-2.sh
@@ -1,5 +1,5 @@
   # Another way to install speedtest-cli
-  wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py 
+  wget --no-check-certificate https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py 
   chmod a+rx speedtest_cli.py 
   sudo mv speedtest_cli.py /usr/local/bin/speedtest-cli 
   sudo chown root:root /usr/local/bin/speedtest-cli
